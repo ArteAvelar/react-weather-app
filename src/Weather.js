6f9c8a6aa472ce97db44142010208dd35a17f5e8
ArteAvelar <arte.avelar@gmail.com>
2022-07-12 18:55:21 +0200

    added weather.css

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
 import React from "react";
+import "./Weather.css";
 
 export default function Weather() {
-  return "hello from Weather";
+  return <div className="Weather">"hello from Weather"</div>;
 }
