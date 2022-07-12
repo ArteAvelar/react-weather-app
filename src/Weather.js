9375681782771b6dc77cc26071c4b0bfb8945b02
ArteAvelar <arte.avelar@gmail.com>
2022-07-12 20:14:14 +0200

    fixing bug

--- src/Weather.js
+++ src/Weather.js
@@ -2,5 +2,20 @@ import React from "react";
 import "./Weather.css";
 
 export default function Weather() {
-  return <div className="Weather">"hello from Weather"</div>;
+  return (
+    <div className="Weather">
+      <h1>Munich</h1>
+      <ul>
+        <li>Monday 7:00</li>
+        <li>Cloudy</li>
+      </ul>
+      <div className="col-6">6°C</div>
+      <div className="col-6">
+        <ul>
+          <li>Humidity</li>
+          <li>Wind</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
