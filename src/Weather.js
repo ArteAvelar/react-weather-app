1f410fb39bf5c9c5f4971c062b1585e1d22eb00a
ArteAvelar <arte.avelar@gmail.com>
2022-07-12 21:01:36 +0200

    Capitalized values

--- src/Weather.js
+++ src/Weather.js
@@ -9,14 +9,14 @@ export default function Weather() {
           <div className="col-9">
             <input
               type="search"
-              placeholder="enter a city"
+              placeholder="Enter a city"
               className="form-control"
             />
           </div>
           <div className="col-3">
             <input
               type="submit"
-              value="search"
+              value="Search"
               className="btn btn-primary w-100"
             />
           </div>
